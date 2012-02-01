8468863703a125cf627763445d1971c71179b098
Franklin Webber <franklin.webber@gmail.com>
2012-02-01 02:47:12 -0800

    TestFlight API updated with more options   * Added reference to the test flight API   * removed validation as the web service does this already   * Added configurable notification values, replace values, and distribution lists   * Distribution lists can be specifed as a comma-delimted string or an array   * Notify and Replace can be specified as true or a string equivalent

--- lib/xcode/builder_testflight.rb
+++ lib/xcode/builder_testflight.rb
@@ -4,22 +4,30 @@ module Xcode
    
     def testflight(parameters = {})
       
+      #
+      # This uses the TestFlight API to send the particular build to the server 
+      # for distribution.
+      # 
+      # @see https://testflightapp.com/api/doc/
+      # 
+      
       testflight_api_token = parameters[:testflight_api_token] || ENV['TESTFLIGHT_API_TOKEN']
       testflight_team_token = parameters[:testflight_team_token] || ENV['TESTFLIGHT_TEAM_TOKEN']
       message = parameters[:message]
-      distribution_list = parameters[:distribution_list] || 'All'
-      
-      if (testflight_api_token.nil? and testflight_team_token.nil?) or message.nil?
-        puts "You must specify a testflight_api_token, testflight_team_token, and a message"
-        return
-      end
+      distribution_list = Array(parameters[:distribution_list]).join(', ')
+      notify = parameters[:notify].to_s =~ /^true$/i
+      replace = parameters[:replace].to_s =~ /^true$/i
       
       cmd = [ "curl -X POST http://testflightapp.com/api/builds.json" ]
       
-      cmd << "-F file=@\"#{ipa_path}\" -F dsym=@\"#{dsym_zip_path}\""
+      cmd << "-F file=@\"#{ipa_path}\""
+      cmd << "-F dsym=@\"#{dsym_zip_path}\""
       cmd << "-F api_token='#{testflight_api_token}'"
       cmd << "-F team_token='#{testflight_team_token}'"
-      cmd << "-F notify=True -F notes=\"#{message}\" -F distribution_lists='#{distribution_list}'"
+      cmd << "-F notify=True" if notify
+      cmd << "-F notes=\"#{message}\""
+      cmd << "-F distribution_lists='#{distribution_list}'" if distribution_list
+      cmd << "-F replace=True" if replace
       
       yield cmd if block_given?
       
