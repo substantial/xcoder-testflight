aa833d18f3ff0c4ac8903f4769435818bb12fde3
Franklin Webber <franklin.webber@gmail.com>
2012-02-01 02:34:38 -0800

    Made the api token or the team token a requirement but not both

--- lib/xcode/builder_testflight.rb
+++ lib/xcode/builder_testflight.rb
@@ -9,7 +9,7 @@ module Xcode
       message = parameters[:message]
       distribution_list = parameters[:distribution_list] || 'All'
       
-      if testflight_api_token.nil? or testflight_team_token.nil? or message.nil?
+      if (testflight_api_token.nil? and testflight_team_token.nil?) or message.nil?
         puts "You must specify a testflight_api_token, testflight_team_token, and a message"
         return
       end
@@ -17,7 +17,8 @@ module Xcode
       cmd = [ "curl -X POST http://testflightapp.com/api/builds.json" ]
       
       cmd << "-F file=@\"#{ipa_path}\" -F dsym=@\"#{dsym_zip_path}\""
-      cmd << "-F api_token='#{testflight_api_token}' -F team_token='#{testflight_team_token}'"
+      cmd << "-F api_token='#{testflight_api_token}'"
+      cmd << "-F team_token='#{testflight_team_token}'"
       cmd << "-F notify=True -F notes=\"#{message}\" -F distribution_lists='#{distribution_list}'"
       
       yield cmd if block_given?
